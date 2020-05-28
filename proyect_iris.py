8cd24f6826e4174b234ff9a4391fa4ab35ad3d38
herny15 <ernesto@gomezchamorro.es>
2020-05-28 21:42:31 +0200

    Update proyect_iris.py
    Add some graphics

--- proyect_iris.py
+++ proyect_iris.py
@@ -3,6 +3,23 @@
 #Import libraries
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+#Function case to select a variable from the dataset
+def data_var(i):
+        switcher={
+                1:'petal_length',
+                2:'petal_width',
+                3:'sepal_length',
+                4:'sepal_width',
+                5:'setosa',
+                6:'versicolor',
+                7:'virginica',
+                8:'All',
+             }
+        return switcher.get(i,"Invalid variabel (1,2,3,4 - 5,6,7)")
 
 #4) Read data from iris.csv
 # Read with the pandas method read_csv and assign to df_iris
@@ -42,6 +59,7 @@ print("\n\n--------------------------\n5) Data Analysis")
 #Métricas estadísticas básicas de cada columna, como la media o desviación típica.
 # Describe data from the whole dataset and grouped by each specie in the 4 indicators 
 
+
 df_statistics_sl = df_iris.groupby('species')['sepal_length'].describe()
 df_statistics_sw = df_iris.groupby('species')['sepal_width'].describe()
 df_statistics_pl = df_iris.groupby('species')['petal_length'].describe()
@@ -59,3 +77,45 @@ print("\n Petal width description:")
 print(df_statistics_pw)
 
 #Gráficas para obtener un mejor conocimiento de los datos, por ejemplo: scatter plots entre pares de variables, distribuciones de cada variable
+# Scatter plots
+print("Enter the first variable to compare\n1 - Petal Length\n2 - Petal Width\n3 - Sepal Length\n4 - Sepal Width")
+x_var = data_var(int(input()))
+
+print("Enter the second variable to compare\n1 - Petal Length\n2 - Petal Width\n3 - Sepal Length\n4 - Sepal Width")
+y_var = data_var(int(input()))
+
+print("Enter the kind of plant\n5 - Setosa\n6 - Versicolor\n7 - Virginica\n8 - All")
+kind_var = data_var(int(input()))
+
+
+if(kind_var == 'All'):
+    df_iris.plot.scatter(x=x_var, y=y_var)
+else:
+    df_iris[df_iris.species == kind_var].plot.scatter(x=x_var, y=y_var)
+
+
+plt.title(kind_var)
+plt.show()
+
+# Same plot with seaborn for all
+sns.lmplot(x=x_var, y= y_var, data=df_iris, palette='Set1')
+plt.show()
+
+# Pie chart of species
+
+df_iris.groupby('species')['species'].count().plot(kind='pie')
+plt.title("Species count")
+plt.show()
+
+# Histogram
+# num_bins = 15
+# n1 = plt.hist(df_iris.sepal_length, num_bins)
+# n2 = plt.hist(df_iris.sepal_width, num_bins)
+# n3 = plt.hist(df_iris.petal_length, num_bins)
+# n4 = plt.hist(df_iris.petal_width, num_bins)
+
+for species_class in df_iris.groupby(['species']).count().index.values:
+    df_iris[df_iris.species == species_class].plot(kind='kde')
+    plt.title(species_class)
+    
+plt.show()
